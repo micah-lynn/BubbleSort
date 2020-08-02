ee2b1f1920e7702b656a80f777db158bb87ee296
Micah Lynn <micah@sabado.com>
2020-08-02 18:13:21 -0400

    Added pseudocode to program

---
+++ bubble_sort.rb
@@ -0,0 +1,7 @@
+#Create a variable with array of unsorted numbers
+
+#Create a method called bubble_sort that takes in the array
+    #For each element in array
+        #If the number on the left is greater than on the right, swap
+
+#Print output to terminal
